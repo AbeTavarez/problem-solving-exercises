d6382b1274e2c5bd54709519f2553cc03dbc6d6a
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-08-26 14:24:47 -0400

    challenge instructions

--- word-occurrence/index.js
+++ word-occurrence/index.js
@@ -1,2 +1,7 @@
-
-
+// Word Occurrence
+// Write an algorithm that counts the amount times a word is found in the phrase.
+// @word - string
+// @phrase - string
+module.exports = (word, phrase) => {
+    // Start coding below this line
+}
